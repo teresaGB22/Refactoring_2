d86b25163842e9372b8ee0b9d1c33e8175b31c29
Teresa Garcia <garcia.beltran.teresa.de.los.angeles@iescamas.es>
2025-01-13 20:57:00 +0100

    Cambios en el ejercicio 1

--- src/refactoring/Ej1.java
+++ src/refactoring/Ej1.java
@@ -18,7 +18,7 @@ public class Ej1 {
 		int opcion = sc.nextInt();
 
 		if (opcion < 1 || opcion > 4) {
-			System.out.println("Opción no válida. Intenta de nuevo.");
+			
 		}
 
 		System.out.println("Ingresa el primer número:");
@@ -27,22 +27,30 @@ public class Ej1 {
 		System.out.println("Ingresa el segundo número:");
 		double numero2 = sc.nextDouble();
 
-		if (opcion == 1) {
-			double resultado = numero1 + numero2;
+		double resultado;
+		switch(opcion) {
+		case 1:
+			resultado = numero1 + numero2;
 			System.out.println("El resultado de la suma es: " + resultado);
-		} else if (opcion == 2) {
-			double resultado = numero1 - numero2;
+			break;
+		case 2:
+			resultado = numero1 - numero2;
 			System.out.println("El resultado de la resta es: " + resultado);
-		} else if (opcion == 3) {
-			double resultado = numero1 * numero2;
+			break;
+		case 3:
+			 resultado = numero1 * numero2;
 			System.out.println("El resultado de la multiplicación es: " + resultado);
-		} else if (opcion == 4) {
+			break;
+		case 4:
 			if (numero2 == 0) {
 				System.out.println("Error: No se puede dividir entre cero.");
 			} else {
-				double resultado = numero1 / numero2;
+				resultado = numero1 / numero2;
 				System.out.println("El resultado de la división es: " + resultado);
 			}
+			break;
+			
+				
 		}
 
 		sc.close();
